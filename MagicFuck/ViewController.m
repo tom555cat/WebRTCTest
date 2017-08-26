770d97cab64d0ae9aad70379759758842952afa2
tom555cat <tongleiming1989@mail.com>
2017-08-26 20:24:36 +0800

    first commit

--- MagicFuck/ViewController.m
+++ MagicFuck/ViewController.m
@@ -6,9 +6,14 @@
 //  Copyright © 2017年 MAC-MiNi. All rights reserved.
 //
 
+@import WebRTC;
 #import "ViewController.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) RTCPeerConnectionFactory *peerFactory;
+@property (nonatomic, strong) RTCMediaStream *mediaStream;
+@property (nonatomic, strong) RTCEAGLVideoView *videoView;
 
 @end
 
@@ -16,14 +21,68 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view.
+    
+    [self createLocalStream];
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(300, 300, 100, 100);
+    [btn addTarget:self action:@selector(playOrUnPlay) forControlEvents:UIControlEventTouchUpInside];
+    btn.backgroundColor = [UIColor redColor];
+    [self.view addSubview:btn];
 }
 
+- (void)playOrUnPlay {
+    static int flag = 0;
+    static int times = 0;
+    if (flag % 2 == 0) {
+        [self play];
+        ++times;
+        NSLog(@"play times: %d", times);
+    } else {
+        [self unplay];
+    }
+    ++flag;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)createLocalStream {
+    self.peerFactory = [[RTCPeerConnectionFactory alloc] init];
+    self.mediaStream = [self.peerFactory mediaStreamWithStreamId:@"Fuck"];
+    
+    // 创建video track
+    RTCAVFoundationVideoSource *source = [self.peerFactory avFoundationVideoSourceWithConstraints:nil];
+    RTCVideoTrack *videoTrack = [self.peerFactory videoTrackWithSource:source trackId:@"FuckVideo"];
+    
+    // 将video track添加到stream中
+    if (videoTrack) {
+        [self.mediaStream addVideoTrack:videoTrack];
+    }
+    
+}
+
+- (void)play {
+    self.videoView = [[RTCEAGLVideoView alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
+    [self.view addSubview:self.videoView];
+    
+    if (self.mediaStream.videoTracks.count > 0) {
+        RTCVideoTrack *videoTrack = self.mediaStream.videoTracks.firstObject;
+        [videoTrack addRenderer:self.videoView];
+    }
+}
+
+- (void)unplay {
+    if (self.mediaStream.videoTracks.count > 0) {
+        RTCVideoTrack *videoTrack = self.mediaStream.videoTracks.firstObject;
+        [videoTrack removeRenderer:self.videoView];
+    }
+    [self.videoView removeFromSuperview];
+}
+
 
 
 @end
